bc696d04e1ce6bc8c7cfb2d219e71cc0cf0511fd
tomerds <46465418+tomerds@users.noreply.github.com>
2022-12-22 17:35:10 -0300

    Update TestToken.sol

--- contracts/TestToken.sol
+++ contracts/TestToken.sol
@@ -4,9 +4,6 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-/**
- * @notice A mintable ERC20
- */
 contract TestToken is ERC20, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
